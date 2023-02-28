df08b838136a7620a1dcfc0318c712c710bc9ee6
alexist2623 <alexist@snu.ac.kr>
2023-03-01 06:35:04 +0900

    RFDC was tested with Zynq simulation

---
+++ RFSoC_Design_RFDC_Test/IP_FILE_04/DAC_Controller/DAC_Controller.ip_user_files/sim_scripts/fifo_generator_1/questa/elaborate.do
@@ -0,0 +1,1 @@
+vopt +acc=npr -l elaborate.log -L xpm -L unisims_ver -L unimacro_ver -L secureip -work xil_defaultlib xil_defaultlib.fifo_generator_1 xil_defaultlib.glbl -o fifo_generator_1_opt
---
+++ RFSoC_Design_RFDC_Test/IP_FILE_04/TEST_00/TEST_00.gen/sources_1/bd/TEST_00_Block/hdl/TEST_00_Block_wrapper.v
@@ -0,0 +1,32 @@
+//Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
+//Date        : Wed Mar  1 03:19:21 2023
+//Host        : LAPTOP-ETOV1IS0 running 64-bit major release  (build 9200)
+//Command     : generate_target TEST_00_Block_wrapper.bd
+//Design      : TEST_00_Block_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module TEST_00_Block_wrapper
+   (dac0_clk_0_clk_n,
+    dac0_clk_0_clk_p,
+    vout00_0_v_n,
+    vout00_0_v_p);
+  input dac0_clk_0_clk_n;
+  input dac0_clk_0_clk_p;
+  output vout00_0_v_n;
+  output vout00_0_v_p;
+
+  wire dac0_clk_0_clk_n;
+  wire dac0_clk_0_clk_p;
+  wire vout00_0_v_n;
+  wire vout00_0_v_p;
+
+  TEST_00_Block TEST_00_Block_i
+       (.dac0_clk_0_clk_n(dac0_clk_0_clk_n),
+        .dac0_clk_0_clk_p(dac0_clk_0_clk_p),
+        .vout00_0_v_n(vout00_0_v_n),
+        .vout00_0_v_p(vout00_0_v_p));
+endmodule
